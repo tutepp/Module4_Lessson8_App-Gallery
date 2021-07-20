e7a2ca46a95ca71fec483dfb9cb10ddf559923ba
Your Name <hoangtu1297hp@gmail.com>
2021-07-20 14:43:59 +0700

    gallery

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImageGalleryModule } from './image-gallery/image-gallery.module';
+import { GalleryConfig } from './image-gallery/token';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ImageGalleryModule,
+  ],
+  providers: [{
+    provide: GalleryConfig ,useValue:3}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/image-gallery/image-gallery.component.ts
@@ -0,0 +1,34 @@
+
+import { Component, OnInit, Input, Inject, Optional } from '@angular/core';
+import { GalleryConfig } from './token';
+
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.css']
+})
+export class ImageGalleryComponent implements OnInit {
+
+  itemWidth: number | undefined;
+  config = 4;
+  constructor(@Inject(GalleryConfig)
+              @Optional() config:number) { if(config){this.config = config;}}
+
+  ngOnInit(): void {
+    this.itemWidth = 100 / this.config;
+  }
+  @Input() src = '';
+
+
+  listImage = [
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=1',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=2',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=3',
+    'https://via.placeholder.com/1280x420/e91e63/ffffff?text=4'
+  ];
+
+
+
+
+}
